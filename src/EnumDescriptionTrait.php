b3722ddfe19334acb859c27acadf3e07d40d04ef
Matthew Butterworth <mbutterworth@cghero.com>
2021-12-21 14:24:40 +0000

    Provide default getDescription() method

--- src/EnumDescriptionTrait.php
+++ src/EnumDescriptionTrait.php
@@ -15,4 +15,33 @@ trait EnumDescriptionTrait
 
         return $selectArray;
     }
+
+    public function getDescription(\BackedEnum $enum): string
+    {
+        if (ctype_upper(preg_replace('/[^a-zA-Z]/', '', $enum->name))) {
+            $key = strtolower($enum->name);
+        }
+
+        return ucfirst(str_replace('_', ' ', self::snakeCase($enum->name)));
+    }
+
+    /**
+     * Convert a string to snake case.
+     *
+     * @param  string  $value
+     * @param  string  $delimiter
+     * @return string
+     */
+    public static function snakeCase(string $value, string $delimiter = '_'): string
+    {
+        $key = $value;
+
+        if (!ctype_lower($value)) {
+            $value = preg_replace('/\s+/u', '', ucwords($value));
+            $value = strtolower((preg_replace('/(.)(?=[A-Z])/u', '$1'.$delimiter, $value)));
+        }
+
+        return $value;
+    }
+
 }
